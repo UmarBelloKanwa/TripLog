8c3e541479b47835cd4f167695d8786b285132dc
Umar BellKo Kanwa <Umarbellokawa@gmail.com>
2025-04-24 09:14:53 +0100

    New project folder structure

---
+++ frontend/src/services/api.js
@@ -0,0 +1,69 @@
+const apiKey = import.meta.env.VITE_OPENROUTESERVICE_API_KEY;
+
+const api = {
+  fetchLocations: async (query = "") => {
+    if (!query.length) {
+      return [];
+    }
+    try {
+      const res = await fetch(
+        `https://api.openrouteservice.org/geocode/search?api_key=${apiKey}&text=${query}`
+      );
+      if (!res.ok) {
+        throw new Error(`Failed to fetch locations: ${res.statusText}`);
+      }
+      const data = await res.json();
+      return data.features.map((place) => ({
+        label: place.properties.label,
+        lat: place.geometry.coordinates[1],
+        lon: place.geometry.coordinates[0],
+      }));
+    } catch /*(error)*/ {
+      //console.error("Error fetching locations:", error);
+      return [];
+    }
+  },
+  reverseGeocode: async (latitude, longitude) => {
+    try {
+      const res = await fetch(
+        `https://api.openrouteservice.org/geocode/reverse?api_key=${apiKey}&point.lat=${latitude}&point.lon=${longitude}`
+      );
+      if (!res.ok) {
+        throw new Error(`Failed to reverse geocode: ${res.statusText}`);
+      }
+      const data = await res.json();
+
+      return data.features.map((place) => ({
+        label: place.properties.label,
+        lat: place.geometry.coordinates[1],
+        lon: place.geometry.coordinates[0],
+      }));
+    } catch /*(error)*/ {
+      //console.error("Error reverse geocoding:", error);
+      return [];
+    }
+  },
+
+  createTrip: async (data) => {
+    data.hours = parseInt(data.hours, 10);
+    try {
+      const response = await fetch("/api/create-trip/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        //throw new Error(`HTTP! Status: ${response.status}`);
+      }
+      return response;
+    } catch (error) {
+      ((err) => {
+        throw err;
+      })(error);
+    }
+  },
+};
+
+export default api;
